d85a0d13cc3c98f201e0f5459ed0a176f440db5f
Ramon Hernandez <hernan43@gmail.com>
2014-11-08 07:19:39 -0500

    Applied nice little GPL stanza

--- OctoStatus/AppDelegate.swift
+++ OctoStatus/AppDelegate.swift
@@ -1,9 +1,19 @@
 //
-//  AppDelegate.swift
-//  OctoStatus
+// OctoStatus is a status bar widget for OS X that gives feedback on what OctoPrint is doing.
+// Copyright (C) 2014  Ray Hernandez
 //
-//  Created by Ramon Hernandez on 11/3/14.
-//  Copyright (c) 2014 Shaclack. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import Cocoa
